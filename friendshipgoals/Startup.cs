146e63333cf3bcb113c256ad27c66a9db003693d
Brandon Bell <brandondavidbell7@gmail.com>
2015-09-19 10:53:39 -0700

    First commit test for FG

---
+++ friendshipgoals/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(friendshipgoals.Startup))]
+namespace friendshipgoals
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
